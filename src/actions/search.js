30860ede7007ca31af16d0bc9d8f12a1c8c35f3f
Greg Kittleson <greg.kittleson@gmail.com>
2019-03-21 11:46:32 -0700

    Implement actions handlers for current video, video list, and search components

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,10 +3,13 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
-var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
+var handleVideoSearch = q => {
+  const options = { YOUTUBE_API_KEY, q, max: 5 };
+  return dispatch => {
+    searchYouTube(options, videos => {
+      dispatch(changeVideoList(videos), changeVideo(videos[0]));
+    });
+  };
 };
 
 export default handleVideoSearch;
