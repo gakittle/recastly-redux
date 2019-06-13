2987fa96b23f33009371a6439fc6ad9cf685d941
Greg Kittleson <greg.kittleson@gmail.com>
2019-06-12 17:37:01 -0700

    Refactor app to functional stateless and remove unused imports/props

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 import App from './components/App.js';
-import searchYouTube from './lib/searchYouTube.js';
-import YOUTUBE_API_KEY from './config/youtube.js';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
+    <App />
   </Provider>,
   document.getElementById('app')
 );
